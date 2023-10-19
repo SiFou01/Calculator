d1acf4283446ab80d43fe3d2af37f054c22f3746
SiFou01 <seifbaghdad01@gmail.com>
2023-10-19 16:40:41 +0100

    Change variable and function names

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let str = "";
+let currentValue = "";
 let result = "";
-let value = "";
+let previousValue = "";
 let operator = "";
-let expression = "";
+let operation = "";
 let click = 0;
 
 let finalResult = document.getElementById("result");
-let written = document.getElementById("write");
+let display = document.getElementById("display");
+
+let btnAdd = document.getElementById("addition");
+let btnEquals = document.getElementById("equals");
 
 let btn0 = document.getElementById("n0");
 let btn1 = document.getElementById("n1");
@@ -19,64 +22,64 @@ let btn7 = document.getElementById("n7");
 let btn8 = document.getElementById("n8");
 let btn9 = document.getElementById("n9");
 
-btn0.addEventListener("click", function(){addToString("0"); write("0")});
-btn1.addEventListener("click", function(){addToString("1"); write("1")});
-btn2.addEventListener("click", function(){addToString("2"); write("2")});
-btn3.addEventListener("click", function(){addToString("3"); write("3")});
-btn4.addEventListener("click", function(){addToString("4"); write("4")});
-btn5.addEventListener("click", function(){addToString("5"); write("5")});
-btn6.addEventListener("click", function(){addToString("6"); write("6")});
-btn7.addEventListener("click", function(){addToString("7"); write("7")});
-btn8.addEventListener("click", function(){addToString("8"); write("8")});
-btn9.addEventListener("click", function(){addToString("9"); write("9")});
+btn0.addEventListener("click", function(){makeValue("0"); write("0")});
+btn1.addEventListener("click", function(){makeValue("1"); write("1")});
+btn2.addEventListener("click", function(){makeValue("2"); write("2")});
+btn3.addEventListener("click", function(){makeValue("3"); write("3")});
+btn4.addEventListener("click", function(){makeValue("4"); write("4")});
+btn5.addEventListener("click", function(){makeValue("5"); write("5")});
+btn6.addEventListener("click", function(){makeValue("6"); write("6")});
+btn7.addEventListener("click", function(){makeValue("7"); write("7")});
+btn8.addEventListener("click", function(){makeValue("8"); write("8")});
+btn9.addEventListener("click", function(){makeValue("9"); write("9")});
 
-function addToString(n) {
-    str += n;
+btnAdd.addEventListener("click", function(){makeOperator(" + "); write(" + ")});
+btnEquals.addEventListener("click", function(){equate()});
+
+function makeValue(n) {
+    currentValue += n;
 }
 
 function write(ex) {
-    if (expression.length < 19) {
-        expression += ex;
+    if  (operation.length < 19) {
+        operation += ex;
         if (click == 1 && ex == operator) {
-            expression = `Ans ${operator}`
+            operation = `Ans ${operator}`
             click--;
         }
         else if (click == 1) {
-            expression = ex;
+            operation = ex;
+            previousValue = "";
+            currentValue = "";
+            result = "";
             click--;
         }
-        written.textContent = expression;
+        display.textContent = operation;
     }
 }
 
-let btnAdd = document.getElementById("o+");
-
-btnAdd.addEventListener("click", function(){makeOperator(" + "); write(" + ")})
-
 function makeOperator(op) {
-    if (!(str == "") && !(value == "")) {
-        two()
+    if (!(currentValue == "") && !(previousValue == "")) {
+        calculate(op)
     }
     else {
-        value = +str;
+        previousValue = +currentValue;
     }
-    str = "";
+    currentValue = "";
     operator = "";
     operator = op;
 }
 
-let btnEquals = document.getElementById("equals");
-
-btnEquals.addEventListener("click", function(){equate()})
-
 function equate() {
     if (operator == " + ") {
-       result = value + +str;
+       result = previousValue + +currentValue;
     }
     finalResult.textContent = result;
     click++;
 }
 
-function two() {
-    value = value + +str;
+function calculate(op) {
+    if (op == " + ") {
+        previousValue = previousValue + +currentValue;
+    }
 }
